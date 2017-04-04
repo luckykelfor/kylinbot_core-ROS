75476cc9309b8ce8b5a28bc8c5393514992a4a50
luckykelfor <luckykelfor@163.com>
2017-04-03 22:22:37 -0700

    Add mimic RC Sub and Pub

--- src/kylinbot_core/src/kylinbot_move.cpp
+++ src/kylinbot_core/src/kylinbot_move.cpp
@@ -4,7 +4,7 @@
 #include <ros/ros.h>
 #include<tf/transform_broadcaster.h>
 #include "std_msgs/String.h"
-#include "std_msgs/Int32.h"
+#include "std_msgs/Int8.h"
 #include<geometry_msgs/Twist.h>
 //#include <main.h>
 #include <nav_msgs/Odometry.h>
@@ -13,6 +13,8 @@
 #include <iostream>
 #define MAF_BUF_LEN 200
 #include<kylinbot_core/kylin_vrc.h>
+#include <main.h>
+
 #define BUF_LEN 256
 using namespace std;
 
@@ -47,18 +49,18 @@ KylinMsg_t kylinMsg;
 PosCalibMsg_t posCalibMsg;
 Sr04sMsg_t sr04sMsg;
 VirtualRC_t vrc;
-
+VirtualRC_t vrc_mimic;
 void initFIFO()
 {
-	//	printf("rx add:%d, rx size: %d\n",rx_fifo.m,rx_fifo.s);
+    //	printf("rx add:%d, rx size: %d\n",rx_fifo.m,rx_fifo.s);
     FIFO_Init(&rx_fifo, rx_buf[0], BUF_LEN);
-	//	printf("rx add:%d, rx size: %d\n",rx_fifo.m,rx_fifo.s);
-	//printf("tx add:%d, tx size: %d\n",tx_fifo.m,tx_fifo.s);
+    //	printf("rx add:%d, rx size: %d\n",rx_fifo.m,rx_fifo.s);
+    //printf("tx add:%d, tx size: %d\n",tx_fifo.m,tx_fifo.s);
     FIFO_Init(&tx_fifo, tx_buf[0], BUF_LEN);
-	//printf("tx add:%d, tx size: %d\n",tx_fifo.m,tx_fifo.s);
+    //printf("tx add:%d, tx size: %d\n",tx_fifo.m,tx_fifo.s);
 }
 
-static void Dnl_ProcKylinMsg(const KylinMsg_t* kylinMsg)
+static void Dnl_ProcKylinMsg(const KylinMsg_t* kylinMsg,ros::Publisher & odom_pub)
 {
     //printf("*************************************KYLIN********************************************\n");
     printf("fs=%x,px=%d,py=%d,pz=%d,pe=%d,pc=%d,vx=%d,vy=%d,vz=%d,ve=%d,vc=%d\n", kylinMsg->cbus.fs,
@@ -66,8 +68,15 @@ static void Dnl_ProcKylinMsg(const KylinMsg_t* kylinMsg)
            kylinMsg->cbus.cv.x, kylinMsg->cbus.cv.y, kylinMsg->cbus.cv.z, kylinMsg->cbus.gv.e, kylinMsg->cbus.gv.c);
 }
 
-static void Dnl_ProcVirtualRC(const VirtualRC_t* vrc)
+static void Dnl_ProcVirtualRC(const VirtualRC_t* vrc,ros::Publisher& vrc_pub)
 {
+    kylinbot_core::kylin_vrc vrc_msg;
+    vrc_msg.frame_id = vrc->frame_id;
+    memcpy(vrc_msg.buf.elems,vrc->buf,6*sizeof(uint8_t));
+
+
+    vrc_pub.publish(vrc_msg);
+
 
 }
 
@@ -77,39 +86,39 @@ void Tri_Init(Tri_t* tri, uint32_t scl)
     tri->cnt = 0;
     tri->dir = 0;
 }
-void PullMsg(ros::Publisher & odom_pub)
+void PullMsg(ros::Publisher & odom_pub,ros::Publisher & vrc_pub)
 //void PullMsg()
 {
-	// Get fifo free space
-	int len = FIFO_GetFree(&rx_fifo);
-	// If fifo free space insufficient, pop one element out
-	if (!len) {
-		uint8_t b;
-		len = FIFO_Pop(&rx_fifo, &b, 1);
-	}
-	// Read input stream according to the fifo free space left
-	len = read_serial(rx_buf[1], len, 255);
-	// Push stream into fifo
-	FIFO_Push(&rx_fifo, rx_buf[1], len);
-	// Check if any message received
-	if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_kylin, &kylinMsg)) {
-		Dnl_ProcKylinMsg(&kylinMsg);
-	}
-	if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_sr04s, &sr04sMsg)) {
-		//Dnl_ProcSr04sMsg(&sr04sMsg);
-	}
-	if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_zgyro, &zgyroMsg)) {
-		//Dnl_ProcZGyroMsg(&zgyroMsg);
-	}
-	if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_pos_calib, &posCalibMsg)) {
-		//Dnl_ProcPosCalibMsg(&posCalibMsg);
-	}
-	if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_vrc, &vrc)) {
-		Dnl_ProcVirtualRC(&vrc);
-	}
-   // printf("Push in PULL tx: add %d size: %d",tx_fifo.m, tx_fifo.s);
-   // cout<<"pull 2.5"<<endl;
-	//printf("Push in PULL tx: add %d size: %d",tx_fifo.m, tx_fifo.s);
+    // Get fifo free space
+    int len = FIFO_GetFree(&rx_fifo);
+    // If fifo free space insufficient, pop one element out
+    if (!len) {
+        uint8_t b;
+        len = FIFO_Pop(&rx_fifo, &b, 1);
+    }
+    // Read input stream according to the fifo free space left
+    len = read_serial(rx_buf[1], len, 255);
+    // Push stream into fifo
+    FIFO_Push(&rx_fifo, rx_buf[1], len);
+    // Check if any message received
+    if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_kylin, &kylinMsg)) {
+        Dnl_ProcKylinMsg(&kylinMsg,odom_pub);
+    }
+    if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_sr04s, &sr04sMsg)) {
+        //Dnl_ProcSr04sMsg(&sr04sMsg);
+    }
+    if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_zgyro, &zgyroMsg)) {
+        //Dnl_ProcZGyroMsg(&zgyroMsg);
+    }
+    if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_pos_calib, &posCalibMsg)) {
+        //Dnl_ProcPosCalibMsg(&posCalibMsg);
+    }
+    if (Msg_Pop(&rx_fifo, rx_buf[1], &msg_head_vrc, &vrc)) {
+        Dnl_ProcVirtualRC(&vrc,vrc_pub);
+    }
+    // printf("Push in PULL tx: add %d size: %d",tx_fifo.m, tx_fifo.s);
+    // cout<<"pull 2.5"<<endl;
+    //printf("Push in PULL tx: add %d size: %d",tx_fifo.m, tx_fifo.s);
     tf::TransformBroadcaster odom_broadcaster,kylinbot_move_broadcaster;
 
     current_time = ros::Time::now();
@@ -146,8 +155,8 @@ Here we'll create a TransformStamped message that we will send out over tf. We w
     kylinbot_move_broadcaster.sendTransform(
             tf::StampedTransform (
 
-            tf::Transform ( tf::Quaternion ( 0, 0, 0 ), tf::Vector3 ( 0.0, 0.0, 0.0 ) ),
-            current_time,"map", "odom" ) );
+                    tf::Transform ( tf::Quaternion ( 0, 0, 0 ), tf::Vector3 ( 0.0, 0.0, 0.0 ) ),
+                    current_time,"map", "odom" ) );
 /*
  在此处我们发布了当前最新base_link坐标系与odem坐标系之间的变换。
 */
@@ -184,15 +193,32 @@ Here we fill in the transform message from our odometry data, and then send the 
 
 void PushMsg()
 {
-	uint32_t len = Msg_Push(&tx_fifo, tx_buf[1], & msg_head_kylin, &txKylinMsg);
-	FIFO_Pop(&tx_fifo, tx_buf[1], len);
-	write_serial(tx_buf[1], len, 255);
+    uint32_t len = Msg_Push(&tx_fifo, tx_buf[1], & msg_head_kylin, &txKylinMsg);
+    FIFO_Pop(&tx_fifo, tx_buf[1], len);
+    write_serial(tx_buf[1], len, 255);
 }
 
-void cmd_velSourceCallback(const std_msgs::Int32 & source )
+void cmd_velSourceCallback(const std_msgs::Int8 & source )
 {
     cmd_vel_source = source.data;
 }
+
+
+void mimicRCCallback(const kylinbot_core::kylin_vrc & vrc_msg)
+{
+
+    if(3 == cmd_vel_source)
+    {
+        vrc_mimic.frame_id = vrc_msg.frame_id;
+        memcpy(vrc_mimic.buf,vrc_msg.buf.elems,6*sizeof(uint8_t));
+        uint32_t len = Msg_Push(&tx_fifo, tx_buf[1], & msg_head_kylin, &vrc_mimic);
+        FIFO_Pop(&tx_fifo, tx_buf[1], len);
+        write_serial(tx_buf[1], len, 255);
+
+    }
+
+}
+
 
 void controlCmd_velFromeNavigationCallback(const geometry_msgs::Twist& cmd_vel)
 {
@@ -204,12 +230,12 @@ void controlCmd_velFromeNavigationCallback(const geometry_msgs::Twist& cmd_vel)
         txKylinMsg.cbus.cv.y = cmd_vel.linear.z;
         txKylinMsg.cbus.cp.z = 2000;
         txKylinMsg.cbus.cv.z = cmd_vel.angular.y;
-		
+
     }
 
-    	    
-		
-	
+
+
+
 
 }
 
@@ -217,7 +243,7 @@ void controlCmd_velFromeNavigationCallback(const geometry_msgs::Twist& cmd_vel)
 void controlCmd_velFromImageProcessingCallback(const geometry_msgs::Twist& cmd_vel)
 {
 //    ROS_INFO("Received a /cmd_vel message!");
-  //  ROS_INFO("Linear Components:[%f,%f,%f]",cmd_vel.linear.x,cmd_vel.linear.y,cmd_vel.linear.z);
+    //  ROS_INFO("Linear Components:[%f,%f,%f]",cmd_vel.linear.x,cmd_vel.linear.y,cmd_vel.linear.z);
     //ROS_INFO("Angular Components:[%f,%f,%f]",cmd_vel.angular.x,cmd_vel.angular.y,cmd_vel.angular.z);
     // Do velocity processing here:
     // Use the kinematics of your robot to map linear and angular velocities into motor commands
@@ -233,12 +259,12 @@ void controlCmd_velFromImageProcessingCallback(const geometry_msgs::Twist& cmd_v
         txKylinMsg.cbus.cv.z = 1000;
         txKylinMsg.cbus.gp.e = cmd_vel.linear.y;
         txKylinMsg.cbus.gv.e = 1000; //cmd_vel.angular.y;
-		 
+
     }
 
 
 //  
-    
+
     // Then set your wheel speeds (using wheel_left and wheel_right as examples)
 //    wheel_left.set_speed(v_l)
 //    wheel_right.set_speed(v_r)
@@ -257,7 +283,6 @@ private:
     ros::Subscriber cmdSub;
     ros::Publisher  actionState_pub;
 
-
 };
 
 
@@ -270,20 +295,22 @@ int main(int argc, char ** argv)
     //ros::Publisher pub = n.advertise<nav_msgs::Odometry_>("kylinbot/Odom",100); //remain buggy.
     ros::Subscriber sub_2 = n.subscribe("cmd_vel",100,controlCmd_velFromeNavigationCallback);
     ros::Subscriber sub_3 = n.subscribe("kylinbot/cmd_vel_source",100,cmd_velSourceCallback);
-	
 
+
+
+    ros::Subscriber sub_4 = n.subscribe("kylinbot/mimicRC",100,mimicRCCallback);
     //Publish Odom
 
     //Virtual Control Command Publisher
-    //ros::Publisher vrc_pub = n.advertise<
+    ros::Publisher vrc_pub = n.advertise<kylinbot_core::kylin_vrc>("vrc",100);
     ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("kylinbo/odom", 100);
-    ros::Rate r(50);
+    ros::Rate r(100);
 
 
-	uint8_t * savePtr = NULL;
+    //uint8_t * savePtr = NULL;
     initFIFO();
-	savePtr = tx_fifo.m;
-    uint32_t cnt = 0;
+    //savePtr = tx_fifo.m;
+    //uint32_t cnt = 0;
     Rmp_Config(&rmp, 50000);
     Maf_Init(&maf, maf_buf, MAF_BUF_LEN);
     Tri_Init(&tri, 2.5e4);
@@ -295,16 +322,13 @@ int main(int argc, char ** argv)
         printf("serial open error!\n");
         return -1;
     }
-    printf("add: %d", tx_fifo.m);
-	
+   // printf("add: %d", tx_fifo.m);
+
     while(ros::ok())
     {
-		//usleep(10000);
-		//cout<<"ok1"<<endl;
         ros::spinOnce();
-		PullMsg(odom_pub);
-        PushMsg(); 
-		//cout<<"ok4"<<endl;
+        PullMsg(odom_pub,vrc_pub);
+        PushMsg();
         r.sleep();
     }
 
